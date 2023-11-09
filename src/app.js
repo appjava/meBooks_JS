d6488e5385ebb1e7298dbd4aa591e88af97f521c
note <appjavadev2019@gmail.com>
2023-11-08 21:57:04 -0500

    Create Functions

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
 console.log("Started App");
+function creaFun(){
+  //console.log("Creating...");
+  //window.alert("Creating...");
+  title = prompt("\nBook Title: ");
+  autor = prompt("\nBook Author: ")
+  title = prompt("\nBook Genere: ");
+  title = prompt("\nBook Pages: ");
+  title = prompt("\nStatus: ");
+  alert("New Book Created!!!");
+
+}
+function reaFun(){
+  //console.log("Reading...");
+  window.alert("Reading...");
+}
+function upFun(){
+  //console.log("Updating...");
+  window.alert("Updating...");
+}
+function delFun(){
+  //console.log("Deleting...");
+  window.alert("Deleting...");
+}
