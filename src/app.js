8d06ff6d7de9211f651d74bd3cc3c185806cb5bf
appjavadev <appjavadev2019@gmail.com>
2023-11-09 18:13:57 -0500

    From objet to table

--- src/app.js
+++ src/app.js
@@ -1,4 +1,29 @@
 console.log("Started App");
+
+const books = [
+  {ID:"1", TITLE:"Frontera Oscura", AUTHOR:"Sabino Cabeza", GENERE:"Fiction", PAGES:"315", STATUS:"Finished"},
+  {ID:"2", TITLE:"El Poder Del Ahora", AUTHOR:"Toel", GENERE:"Help", PAGES:"200", STATUS:"Waiting"},
+  {ID:"3", TITLE:"Time Machine", AUTHOR:"Wells", GENERE:"Fiction", PAGES:"500", STATUS:"Reading"}
+]
+
+let titulo = document.getElementById('titles');
+let output = '';
+
+for (let i=0; i<books.length; i++){
+  output = output + `
+  <tr>
+  <td>${books[i].ID}</td>
+  <td>${books[i].TITLE}</td>
+  <td>${books[i].AUTHOR} </td>
+  <td>${books[i].GENERE}</td>
+  <td>${books[i].PAGES}</td>
+  <td>${books[i].STATUS}</td>
+  </tr>`;
+
+  titulo.innerHTML = output;
+}
+
+
 function creaFun(){
   //console.log("Creating...");
   //window.alert("Creating...");
