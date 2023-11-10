f6aed6e6d81eab401e601b3384dbb2b1514cbf4b
appjavadev <appjavadev2019@gmail.com>
2023-11-09 20:01:27 -0500

    Add Items to Array

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ console.log("Started App");
 let bodyTable = document.getElementById('bodyTable');
 let bodyTableContent = '';
 
+function readJSONFile(){
+
 //------------ Read JSON File ----------------------------
 fetch('src/books.json').then(response => response.json())
   .then(data => {
@@ -18,21 +20,41 @@ fetch('src/books.json').then(response => response.json())
       </tr>`;
       bodyTable.innerHTML = bodyTableContent;
     };
-    jsonBooks = JSON.stringify(data);
+    arrayBooks = data;
+    jsonBooks = JSON.stringify(arrayBooks);
+    
+
   })
 //----------------------------------------------------------
-
-
+}
+readJSONFile();
 
 function creaFun(){
+  
   //console.log("Creating...");
   //window.alert("Creating...");
   title = prompt("\nBook Title: ");
-  autor = prompt("\nBook Author: ")
+  author = prompt("\nBook Author: ")
   genere = prompt("\nBook Genere: ");
   pages = prompt("\nBook Pages: ");
   state = prompt("\nStatus: ");
-  alert("New Book Created!!!");
+  id = arrayBooks.length + 1;
+  const newBook = {ID:id, TITLE:title, AUTHOR:author, GENERE:genere, PAGES:pages, STATUS:state};
+  arrayBooks.push(newBook);
+  alert("New Book Created!!!\n" + JSON.stringify(arrayBooks));
+  bodyTableContent = '';
+  for (let i=0; i<arrayBooks.length; i++){
+    bodyTableContent = bodyTableContent + `
+    <tr>
+    <td>${arrayBooks[i].ID}</td>
+    <td>${arrayBooks[i].TITLE}</td>
+    <td>${arrayBooks[i].AUTHOR} </td>
+    <td>${arrayBooks[i].GENERE}</td>
+    <td>${arrayBooks[i].PAGES}</td>
+    <td>${arrayBooks[i].STATUS}</td>
+    </tr>`;
+    bodyTable.innerHTML = bodyTableContent;
+  };
 
 }
 function reaFun(){
