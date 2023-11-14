368f1a06f909c25dd7221a91404d3ec682e718a3
javaair <appjavadev2019@gmail.com>
2023-11-13 21:08:45 -0500

    Save file

--- src/app.js
+++ src/app.js
@@ -78,4 +78,20 @@ function loadFun(){
 function saveFun(){
   //console.log("Reading...");
   window.alert("Saved All Books\n" + jsonBooks);
+
+  // Start file download.
+  download("books.json",jsonBooks);
 }
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
